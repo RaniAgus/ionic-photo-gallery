458724137b0eb5d427adafc0f40776d657b5c66a
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-28 08:53:39 -0300

    adding mobile

--- src/utils/photoStorage.ts
+++ src/utils/photoStorage.ts
@@ -1,6 +1,8 @@
 import { Photo } from "@capacitor/camera";
-import { Directory, Filesystem } from "@capacitor/filesystem";
+import { Capacitor } from "@capacitor/core";
+import { Directory, Filesystem, WriteFileResult } from "@capacitor/filesystem";
 import { Preferences } from "@capacitor/preferences";
+import { isPlatform } from "@ionic/core";
 
 export const PHOTO_STORAGE = 'photos';
 
@@ -9,8 +11,13 @@ export interface UserPhoto {
   webviewPath?: string;
 }
 
-const base64FromPath = async (path: string): Promise<string> => {
-  const response = await fetch(path);
+const base64FromFileSystem = async (path: string): Promise<string> => {
+  const file = await Filesystem.readFile({ path });
+  return file.data;
+}
+
+const base64FromWebPath = async (webPath: string): Promise<string> => {
+  const response = await fetch(webPath);
   const blob = await response.blob();
 
   return new Promise((resolve, reject) => {
@@ -27,20 +34,41 @@ const base64FromPath = async (path: string): Promise<string> => {
   });
 }
 
+export const createUserPhotoFromSavedFile = (savedFile: WriteFileResult): UserPhoto => {
+  // Display the new image by rewriting the 'file://' path to HTTP
+  // Details: https://ionicframework.com/docs/building/webview#file-protocol
+  return {
+    filepath: savedFile.uri,
+    webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+  };
+}
+
+export const createUserPhotoFromMemory = (photo: Photo, fileName: string): UserPhoto => {
+  // Use webPath to display the new image instead of base64 since it's
+  // already loaded into memory
+  return {
+    filepath: fileName,
+    webviewPath: photo.webPath,
+  };
+}
+
 export const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-  const base64Data = await base64FromPath(photo.webPath!);
+  // 'hybrid' detecta si está corriendo sobre Cordova o Capacitor
+  const base64Data = isPlatform('hybrid')
+    ? await base64FromFileSystem(photo.path!)
+    : await base64FromWebPath(photo.webPath!);
+
   const savedFile = await Filesystem.writeFile({
     path: fileName,
     data: base64Data,
     directory: Directory.Data,
   });
 
-  // Uso webPath para mostrar la imagen en vez de base64 porque la imagen ya
-  // está en memoria
-  return {
-    filepath: fileName,
-    webviewPath: photo.webPath,
-  };
+  // Si estoy en web no necesito el archivo guardado porque la imagen ya está
+  // cargada en memoria
+  return isPlatform('hybrid')
+    ? createUserPhotoFromSavedFile(savedFile)
+    : createUserPhotoFromMemory(photo, fileName);
 }
 
 export const loadWebviewPathFromPhoto = async (photo: UserPhoto): Promise<UserPhoto> => {
@@ -49,14 +77,6 @@ export const loadWebviewPathFromPhoto = async (photo: UserPhoto): Promise<UserPh
     directory: Directory.Data,
   });
 
-  console.log({ filepath: photo.filepath, data: file.data });
-
-  // En mobile vamos a poder usar la ruta desde el filesystem
-  // directamente en el image tag `<img src="..."/>`
-
-  // En cambio, en la web tenemos que leer la imagen en formato base64, porque
-  // la API de Ionic los guarda de esa forma usando IndexedDB
-  // Ref: https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
   return {
     filepath: photo.filepath,
     webviewPath: `data:image/jpeg;base64,${file.data}`,
@@ -66,5 +86,13 @@ export const loadWebviewPathFromPhoto = async (photo: UserPhoto): Promise<UserPh
 export const loadSaved = async (): Promise<UserPhoto[]> => {
   const { value } = await Preferences.get({ key: PHOTO_STORAGE });
   const photosInPreferences = (value ? JSON.parse(value) : []) as UserPhoto[];
-  return Promise.all(photosInPreferences.map(loadWebviewPathFromPhoto));
+
+  // En mobile vamos a poder usar la ruta desde el filesystem
+  // directamente en el image tag `<img src="..."/>`
+  return isPlatform('hybrid')
+    ? photosInPreferences
+  // En cambio, en la web tenemos que leer la imagen en formato base64, porque
+  // la API de Ionic los guarda de esa forma usando IndexedDB
+  // Ref: https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API
+    : Promise.all(photosInPreferences.map(loadWebviewPathFromPhoto));
 }
